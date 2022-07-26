9d54bb2e007ed0dd74e1c38868a6f32882b375e3
wesley souza <wesley.souza@atos.net>
2022-07-26 14:09:39 -0300

    repo att

--- zabbix_agent_install.py
+++ zabbix_agent_install.py
@@ -5,16 +5,16 @@ so = system.lower()
 print(so)
 
 if "ubuntu" in so:
-    subprocess.call(["ansible-playbook", "ubuntu-agent.yaml"])
+    subprocess.call(["ansible-playbook", "ubuntu-agent.yaml", "-K"])
 elif "debian" in so:
-    subprocess.call(["ansible-playbook", "debian-agent.yaml"])
+    subprocess.call(["ansible-playbook", "debian-agent.yaml", "-K"])
 elif "redhat" in so: 
-    subprocess.call(["ansible-playbook", "rhel-agent.yaml"])
+    subprocess.call(["ansible-playbook", "rhel-agent.yaml", "-K"])
 elif "red hat" in so: 
-    subprocess.call(["ansible-playbook", "rhel-agent.yaml"])
+    subprocess.call(["ansible-playbook", "rhel-agent.yaml", "-K"])
 elif "centos" in so:
-    subprocess.call(["ansible-playbook", "centos-agent.yaml"])
+    subprocess.call(["ansible-playbook", "centos-agent.yaml", "-K"])
 elif "suse" in so:
-    subprocess.call(["ansible-playbook", "suse-agent.yaml"])
+    subprocess.call(["ansible-playbook", "suse-agent.yaml", "-K"])
 else:
     ("SO non supported")
